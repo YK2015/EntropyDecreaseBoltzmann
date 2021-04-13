f9ee160ca3490681af57de8cab5579a681708df4
iamkuang <kuangyoung0107@gmail.com>
2021-04-13 17:16:30 +0800

    fft3d

---
+++ CalQ/fft3d.h
@@ -0,0 +1,54 @@
+/**
+* Copyright (C) 2021 All rights reserved.
+* @file fft3d.h
+* @brief  input real data, output frequency data, and vice visa
+* @author Yang Kuang (matkuan@nus.edu.sg)
+* @date 2021-04-13
+* @return 
+*/
+
+#ifndef __FFT3D_H__
+#define __FFT3D_H__
+
+#include <cmath>
+#include <complex>
+#include <fftw3.h>
+
+class fft3d
+{
+	public:
+		fftw3d(){};
+		~fftw3d(){};
+
+	public:
+		void fft(int Nx, int Ny, int Nz, const fftw_complex& re , fftw_complex& fre){
+			fftw_plan p;
+			p = fftw_plan_dft_3d(Nx,Ny,Nz, re, fre, FFTW_FORWARD, FFTW_ESTIMATE);
+			fftw_execute(p);
+			fftw_destroy_plan(p);
+		}
+
+		void ifft(int Nx, int Ny, int Nz, fftw_complex& re , const fftw_complex& fre){
+			fftw_plan p;
+			p = fftw_plan_dft_3d(Nx,Ny,Nz, fre, re, FFTW_FORWARD, FFTW_ESTIMATE);
+			fftw_execute(p);
+			fftw_destroy_plan(p);
+
+			/// normalization
+		  int N=Nx*Ny*Nz;
+			int k = 0;
+			for(int i= 0; i < Nx; ++ i)
+				for(int j = 0; j < Ny; ++j)
+					for(int l = 0; l < Ny; ++l){
+						re[k] *= 1./N;++k;
+					}
+		}
+}
+;
+
+
+
+
+
+#endif ///FFT3D_H__
+
